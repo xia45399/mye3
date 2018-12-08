17bb104a2fefa6cfa9c2d32e0309638e000b6a7e
xia45399 <453993887@qq.com>
2018-12-08 14:34:06 +0800

    redis不设置密码 登录系统设置回调参数到model 设置cookie域名去掉最前面的.

--- mye3-sso-web/src/main/java/com/summer/sso/controller/LoginController.java
+++ mye3-sso-web/src/main/java/com/summer/sso/controller/LoginController.java
@@ -3,9 +3,9 @@ package com.summer.sso.controller;
 import com.summer.common.utils.CookieUtils;
 import com.summer.common.utils.E3Result;
 import com.summer.mye3.sso.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,7 +24,8 @@ public class LoginController {
     private String TOKEN_KEY;
 
     @RequestMapping("/page/login")
-    public String showLogin() {
+    public String showLogin(Model model, String redirect) {
+        model.addAttribute("redirect", redirect);
         return "login";
     }
 
